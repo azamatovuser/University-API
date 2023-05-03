fe2690e20f7becda3cd885494f83dd3198b7e21c
Azamat <azamatovuser@gmail.com>
2023-05-04 03:00:19 +0500

    fixing the issues

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+import requests.cookies
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -25,11 +26,32 @@ class LoginAPIView(APIView):
         if user is None:
             raise AuthenticationFailed('User not found!')
 
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+
         payload = {'id': user.id,
                    'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
                    'iat': datetime.datetime.utcnow()}
-        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
 
-        return Response({
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
             'jwt': token
-        })
+        }
+        return response
+    
+    
+class ProfileAPIView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError :
+            raise AuthenticationFailed('Unauthenticated!')
+        user = Profile.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
